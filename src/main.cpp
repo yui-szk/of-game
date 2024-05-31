beef96de647be6e53c4ba9aa3a69c5e94b569412
yui-szk <yuisuzuki.017@gmail.com>
2024-05-31 15:09:44 +0900

    first format

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,18 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
+int main()
+{
 
-	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+	// Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
 	ofGLWindowSettings settings;
 	settings.setSize(1024, 768);
-	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+	settings.windowMode = OF_WINDOW; // can also be OF_FULLSCREEN
 
 	auto window = ofCreateWindow(settings);
 
 	ofRunApp(window, make_shared<ofApp>());
 	ofRunMainLoop();
-
 }
 
 // 探索型ゲーム
