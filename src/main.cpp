9a4eefdc44b588bb0e596a2d9b82c6b5e0d7348b
yui-szk <yuisuzuki.017@gmail.com>
2024-06-07 14:58:48 +0900

    不要なコメント削除

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
-#include "ofMain.h"
 #include "ofApp.h"
+#include "ofMain.h"
 
 //========================================================================
-int main()
-{
+int main() {
+  // Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
+  ofGLWindowSettings settings;
+  settings.setSize(1024, 768);
+  settings.windowMode = OF_WINDOW;  // can also be OF_FULLSCREEN
 
-	// Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
-	ofGLWindowSettings settings;
-	settings.setSize(1024, 768);
-	settings.windowMode = OF_WINDOW; // can also be OF_FULLSCREEN
+  auto window = ofCreateWindow(settings);
 
-	auto window = ofCreateWindow(settings);
-
-	ofRunApp(window, make_shared<ofApp>());
-	ofRunMainLoop();
+  ofRunApp(window, make_shared<ofApp>());
+  ofRunMainLoop();
 }
-
-// 探索型ゲーム
-// player wasd操作
-// enemy random
-// enemyに当たるとplayerのライフが減少
